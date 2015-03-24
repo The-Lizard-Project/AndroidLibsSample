9850066ec3f87a2f0b98bca705934b83ae14ae57
Michał Górski <mgorski@future-processing.com>
2015-03-24 22:30:51 +0100

    add butterknife listeners

--- app/src/main/java/com/lizardproject/androidlibssample/libs/SampleLibsActivity.java
+++ app/src/main/java/com/lizardproject/androidlibssample/libs/SampleLibsActivity.java
@@ -2,8 +2,8 @@ package com.lizardproject.androidlibssample.libs;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.lizardproject.androidlibssample.R;
 
@@ -11,11 +11,11 @@ import java.util.Arrays;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
+import butterknife.OnItemClick;
 
 public class SampleLibsActivity extends Activity {
 
-	@InjectView(R.id.button)
-	protected Button button;
 	@InjectView(R.id.list)
 	protected ListView list;
 
@@ -28,5 +28,15 @@ public class SampleLibsActivity extends Activity {
 		list.setAdapter(new SampleLibsListAdapter(this, Arrays.asList("SampleText1", "SampleText2", "SampleText3")));
 
 		getFragmentManager().beginTransaction().add(R.id.fragment, new SampleLibsFragment(), "tag").commit();
+	}
+
+	@OnClick(R.id.button)
+	protected void onButtonClicked() {
+		Toast.makeText(this, "OnClickListener called by Butterknife", Toast.LENGTH_SHORT).show();
+	}
+
+	@OnItemClick(R.id.list)
+	protected void onListItemClicked(int position) {
+		Toast.makeText(this, position + "\titem clicked - OnItemClickListener called by Butterknife", Toast.LENGTH_SHORT).show();
 	}
 }
--- app/src/main/java/com/lizardproject/androidlibssample/normal/SampleNormalActivity.java
+++ app/src/main/java/com/lizardproject/androidlibssample/normal/SampleNormalActivity.java
@@ -2,8 +2,11 @@ package com.lizardproject.androidlibssample.normal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.lizardproject.androidlibssample.R;
 
@@ -25,5 +28,19 @@ public class SampleNormalActivity extends Activity {
 		list.setAdapter(new SampleNormalListAdapter(this, Arrays.asList("SampleText1", "SampleText2", "SampleText3")));
 
 		getFragmentManager().beginTransaction().add(R.id.fragment, new SampleNormalFragment(), "tag").commit();
+
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(SampleNormalActivity.this, "Normal OnClickListener", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Toast.makeText(SampleNormalActivity.this, position + "\titem clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 }
